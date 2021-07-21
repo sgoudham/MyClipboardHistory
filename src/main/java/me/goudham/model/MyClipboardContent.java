1ef16260c819b7197f01a23179ebb7ba6442927d
sgoudham <sgoudham@gmail.com>
2021-07-21 01:35:17 +0100

    Add equals() & hashcode() methods

--- src/main/java/me/goudham/model/MyClipboardContent.java
+++ src/main/java/me/goudham/model/MyClipboardContent.java
@@ -1,17 +1,32 @@
 package me.goudham.model;
 
+import java.util.Objects;
+
 public class MyClipboardContent<T> {
-	private T content;
+    private T content;
 
-	public MyClipboardContent(T content) {
-		this.content = content;
-	}
+    public MyClipboardContent(T content) {
+        this.content = content;
+    }
 
-	public void setContent(Object content) {
-		this.content = (T) content;
-	}
+    public void setContent(Object content) {
+        this.content = (T) content;
+    }
 
-	public T getContent() {
-		return content;
-	}
+    public T getContent() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyClipboardContent<?> that = (MyClipboardContent<?>) o;
+        return Objects.equals(content, that.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(content);
+    }
 }
